e6f2e8b0561c6ad4558cb50afe0ab747eb69b53b
moonlightbay <807014226@qq.com>
2024-05-06 16:05:46 +0800

    try task3

---
+++ task_3/processor.v
@@ -0,0 +1,109 @@
+
+/*处理器 Z 说明：
+1. 处理器 Z 只包括取指、译码、执行、写回、更新 PC 五个流水级操作。
+2. 处理器 Z 将指令字长和数据字长均固定为 4 个字节 (32bit)。因此对于取指操作，每个时钟周期只需要从 ram 中读取 4 个字节的指令即可。
+3. 指令集说明：处理器 Z 只包括 5 条指令，分别为 IRMOV， ADD， SUB， AND， XOR。
+4. 寄存器说明：处理器 Z 的片上寄存器只包括 6 个 32 位寄存器，分别命名为 %r0， %r1， ...， %r5。
+5. 指令编码说明：处理器 Z 的指令长度固定为 4 个字节(32bit)
+    - 第一个字节(31:24)表示指令类型，高 4 位表示指令代码，低 4 位表示指令功能 ifun；
+    - 第二个字节(23:16)表示操作数所在寄存器的 ID，其中高 4 位表示 rA，低 4 位表示 rB；
+    - 最后两个字节(15:0)表示立即数 valC，本设计中所有立即数均以 16 进制表示。*/
+
+/*功能描述：当 working 有效时，处理器 Z 在每个时钟周期从存储器中取出一
+条 32 位的指令，并解析指令；当 working 无效时，并且在写使能信号
+wEn 有效时， 通过 addr 和 wDat 向指令存储器中写入数据。*/
+
+/*补充功能描述： 无论取指结果如何，通过将寄存器 ID 为 rA 的寄存器数据输
+出到总线 valA 上，将寄存器 ID 为 rB 的寄存器数据输出到总线 valB 上。(寄存器
+文件提供的 dstE 和 valE 接口将在任务 4 中使用，本任务中可以悬空)*/
+
+module processor(
+    input wire clock,
+    input wire[8:0] addr,      //外部提供的地址
+    input wire wEn,
+    input wire[31:0] wDat,    //wDat is for writting data to ram         
+    input wire working,    
+    output [31:0] valA,
+    output [31:0] valB
+);
+    /*PC*/
+    reg [15:0] pc = 0;         // Program counter
+
+    /*指令*/
+    wire [31:0] instr;         // Instruction
+    // reg [3:0] icode, ifun, rA, rB;     // Instruction code, function code, rA, rB
+    // reg[15:0] valC = 0;         //立即数
+    // icode <= instr[31:28];
+    // ifun <= instr[27:24];
+    // rA <= instr[23:20];
+    // rB <= instr[19:16];
+    // valC <= instr[15:0];
+    
+    /*RAM*/
+    wire [8:0] ram_addr;       // RAM address
+    wire ram_wEn;              // RAM write enable
+    assign ram_wEn = wEn & ~working;  //当working有效时，不允许写入ram
+    assign ram_addr = ram_wEn ? addr : pc;  //当ram_wEn有效时，地址为addr，否则为pc
+    
+    /*寄存器文件*/
+    reg[3:0] dstE, dstM;      //目的寄存器
+    reg[31:0] valE, valM;     //写入寄存器的数据
+    reg[3:0] srcA, srcB;      //源寄存器
+
+    //实例化RAM
+    ram inst_ram(
+        .clock(clock),            //clock信号
+        .addr(ram_addr),     
+        .wEn(ram_wEn),       //ram的写使能信号
+        .wDat(wDat),             //写入ram的数据
+        .rEn(working),           //读使能信号
+        .rDat(instr)             //读取来自ram的数据
+    );
+
+    //实例化寄存器文件
+    regfile regfile1(
+        .dstE(dstE),
+        .valE(valE),
+        .dstM(dstM),
+        .valM(valM),
+        .rA(srcA),
+        .rB(srcB),
+        .reset(0),
+        .clock(clock),
+        .valA(valA),
+        .valB(valB),
+        .r0(),
+        .r1(),
+        .r2(),
+        .r3(),
+        .r4(),
+        .r5()
+    );
+
+    always @(posedge clock) begin
+        if (working && instr != 32'h0) begin   //取指令
+            if (instr[31:28] == 4'b0001 && instr[27:24] == 4'b0000) begin   //IRMOV, rB -> valC
+                dstM <= instr[19:16];      //rB
+                valM <= instr[15:0];       //valC
+            end
+            else if (instr[31:28] == 4'b0010 && instr[27:24] == 4'b0000) begin   //ADD,%rA ->valA, %rB -> valB
+                srcA <= instr[23:20];      //rA
+                srcB <= instr[19:16];      //rB
+            end
+            else if (instr[31:28] == 4'b0010 && instr[27:24] == 4'b0001) begin   //SUB,%rA ->valA, %rB -> valB
+                srcA <= instr[23:20];      //rA
+                srcB <= instr[19:16];      //rB
+            end
+            else if (instr[31:28] == 4'b0011 && instr[27:24] == 4'b0010) begin   //AND,%rA ->valA, %rB -> valB
+                srcA <= instr[23:20];      //rA
+                srcB <= instr[19:16];      //rB
+            end
+            else if (instr[31:28] == 4'b0011 && instr[27:24] == 4'b0011) begin   //XOR,%rA ->valA, %rB -> valB
+                srcA <= instr[23:20];      //rA
+                srcB <= instr[19:16];      //rB
+            end
+
+            pc <= pc + 1;   //更新PC
+        end
+    end
+endmodule
---
+++ task_3/regfile.v
@@ -0,0 +1,79 @@
+/*功能描述：采用同步复位，当 reset 有效，将寄存器 r0-r5 的数据清零；每个
+时钟周期，将数据 valE 写入寄存器 ID 为 dstE 的寄存器；每个时钟周期，将数据
+valM 写入寄存器 ID 为 dstM 的寄存器;每个时钟周期，将寄存器 ID 为 srcA 的寄存
+器数据输出到 valA；每个时钟周期，将寄存器 ID 为 srcB 的寄存器数据输出到
+valB；*/
+/*每个时钟周期输出寄存器 r0-r5 的数据。*/
+
+module regfile(
+    input wire[3:0] dstE,
+    input wire[31:0] valE,
+    input wire[3:0] dstM,
+    input wire[31:0] valM,
+    input wire[3:0] rA,    //src A
+    input wire[3:0] rB,    //src B
+    input wire reset,
+    input wire clock,
+    output reg [31:0] valA,
+    output reg [31:0] valB,
+    output reg [31:0] r0,
+    output reg [31:0] r1,
+    output reg [31:0] r2,
+    output reg [31:0] r3,
+    output reg [31:0] r4,
+    output reg [31:0] r5
+);
+
+always @(posedge clock) begin
+    if (reset) begin
+        r0 <= 0;
+        r1 <= 0;
+        r2 <= 0;
+        r3 <= 0;
+        r4 <= 0;
+        r5 <= 0;
+    end
+    else begin
+        case(dstE)
+            4'b0000: r0 <= valE;
+            4'b0001: r1 <= valE;
+            4'b0010: r2 <= valE;
+            4'b0011: r3 <= valE;
+            4'b0100: r4 <= valE;
+            4'b0101: r5 <= valE;
+            default: ;// do nothing
+        endcase;
+
+        case(dstM)
+            4'b0000: r0 <= valM;
+            4'b0001: r1 <= valM;
+            4'b0010: r2 <= valM;
+            4'b0011: r3 <= valM;
+            4'b0100: r4 <= valM;
+            4'b0101: r5 <= valM;
+            default: ;// do nothing
+        endcase;    
+
+        case(rA)
+            4'b0000: valA <= r0;
+            4'b0001: valA <= r1;
+            4'b0010: valA <= r2;
+            4'b0011: valA <= r3;
+            4'b0100: valA <= r4;
+            4'b0101: valA <= r5;
+            default: ;// do nothing
+        endcase;
+
+        case(rB)
+            4'b0000: valB <= r0;
+            4'b0001: valB <= r1;
+            4'b0010: valB <= r2;
+            4'b0011: valB <= r3;
+            4'b0100: valB <= r4;
+            4'b0101: valB <= r5;
+            default: ;// do nothing
+        endcase; 
+    end
+end
+
+endmodule
